483128b2ad0a2213a19f8fc62695128446a0cc58
JCHETAN26 <chetanjainnahar@gmail.com>
2025-01-23 14:02:15 -0700

    folder structure added

--- template.py
+++ template.py
@@ -6,8 +6,8 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(messge)s:')
 
 project_name = "NLP_Text_Summarizer"
 
-list_of_file = [
-    ".github/workflows/.gitkeep",
+list_of_files = [
+    
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -25,19 +25,19 @@ list_of_file = [
     "Dockerfile", 
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
-
+    "research/trials.ipynb",
+    ".github/workflows/.gitkeep",
 
 ]
 
 
 
-for filepath in list_of_file:
-    file_path = Path(filepath)
+for filepath in list_of_files:
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
     if filedir != "":
-        os.makedirs(file_path, exist_ok=True)
+        os.makedirs(filepath, exist_ok=True)
         logging.info(f"Created directory: {filedir} for the file {filename}")
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath, "w") as f:
